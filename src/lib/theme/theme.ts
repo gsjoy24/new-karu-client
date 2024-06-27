45e308b740ed353c10b5d4cb39e5f999580d7c99
gsjoy24 <gour.joy24@gmail.com>
2024-06-27 11:30:38 +0600

    first commit

---
+++ src/lib/theme/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#29A56C'
+		},
+		secondary: {
+			main: '#4A4A4A'
+		}
+	},
+	components: {
+		MuiButton: {
+			defaultProps: {
+				variant: 'contained'
+			}
+		}
+	}
+});
