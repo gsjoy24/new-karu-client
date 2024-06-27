90e7e398dd74da5ea2e3f1026eba7d8c50c1eea2
gsjoy24 <gour.joy24@gmail.com>
2024-06-27 11:38:38 +0600

    config done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		protocols: 'https',
+		hostname: '**'
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/lib/theme/theme.ts
+++ src/lib/theme/theme.ts
@@ -14,6 +14,11 @@ export const theme = createTheme({
 			defaultProps: {
 				variant: 'contained'
 			}
+		},
+		MuiContainer: {
+			defaultProps: {
+				maxWidth: 'lg'
+			}
 		}
 	}
 });
