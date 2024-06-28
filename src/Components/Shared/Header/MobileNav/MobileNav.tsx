c2f0eaf1d3dc668e3bb3487c562c3cfab238370e
gsjoy24 <gour.joy24@gmail.com>
2024-06-28 23:07:21 +0600

    modified mobile nav

--- src/Components/Shared/Header/MobileNav/MobileNav.tsx
+++ src/Components/Shared/Header/MobileNav/MobileNav.tsx
@@ -1,20 +1,23 @@
 'use client';
 import logo from '@/assets/Karukon-logo.png';
-import { Chip, Divider, IconButton, Stack } from '@mui/material';
+import { Chip, IconButton, Stack } from '@mui/material';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Image from 'next/image';
 import Link from 'next/link';
 import * as React from 'react';
 import { CgMenuRightAlt } from 'react-icons/cg';
+import { CiLocationOn } from 'react-icons/ci';
 import { FaRegUser } from 'react-icons/fa';
-import { IoIosHeartEmpty } from 'react-icons/io';
+import { GiFlexibleLamp } from 'react-icons/gi';
 import { IoCartOutline, IoClose } from 'react-icons/io5';
+import { LuPhoneCall } from 'react-icons/lu';
+import { MdOutlineArrowOutward } from 'react-icons/md';
+import SocialSection from '../../SocialSection';
 import SearchProduct from '../SearchProduct';
 
 const MobileNav = () => {
 	const [open, setOpen] = React.useState(false);
-	const wishlist = 5;
 	const cart = 2;
 	const mobileNavLinks = [
 		{ name: 'Home', href: '/' },
@@ -39,13 +42,28 @@ const MobileNav = () => {
 		}
 	];
 
+	const currentYear = new Date().getFullYear();
+
 	const toggleDrawer = (newOpen: boolean) => () => {
 		setOpen(newOpen);
 	};
 
 	const DrawerList = (
-		<Box sx={{ width: 350 }} role='presentation'>
-			<Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 2 }}>
+		<Box sx={{ width: 350 }} role='presentation' px={2}>
+			<Box
+				sx={{
+					display: 'flex',
+					justifyContent: 'space-between',
+					alignItems: 'center',
+					p: 2,
+					borderBottom: '1px solid #455a6477',
+					mb: 3,
+					position: 'sticky',
+					top: 0,
+					bgcolor: 'background.paper',
+					zIndex: 1
+				}}
+			>
 				<Image src={logo} alt='logo' width={80} height={80} />
 				<IconButton
 					size='small'
@@ -59,21 +77,70 @@ const MobileNav = () => {
 					<IoClose size={22} />
 				</IconButton>
 			</Box>
-			<Divider
-				sx={{
-					mb: 2
-				}}
-			/>
 
 			{/* search bar */}
 			<SearchProduct />
-			<Stack direction='column' gap={2} p={2}>
+
+			{/* links */}
+			<Stack direction='column' gap={2} p={2} mt={2}>
 				{mobileNavLinks.map((link, index) => (
 					<Link key={index} href={link.href}>
-						{link.name}
+						<Stack
+							direction='row'
+							justifyContent='space-between'
+							align-items='center'
+							sx={{
+								'&:hover': {
+									color: 'primary.main'
+								},
+								transition: 'all 0.3s',
+								borderBottom: '1px solid #f0f0f0'
+							}}
+						>
+							<span>{link.name}</span>
+							<MdOutlineArrowOutward />
+						</Stack>
 					</Link>
 				))}
 			</Stack>
+
+			{/* info */}
+			<Stack
+				gap={1}
+				sx={{
+					border: '1px solid #f0f0f0',
+					borderRadius: 2,
+					p: 2,
+					my: 2,
+					fontSize: 14
+				}}
+			>
+				<Stack direction='row' gap={2} alignItems='center'>
+					<CiLocationOn size={22} />
+					<span>232 A, Dim para, Dhaka</span>
+				</Stack>
+				<Stack direction='row' gap={2} alignItems='center'>
+					<LuPhoneCall size={22} />
+					<span>+1234567890</span>
+				</Stack>
+				<Stack direction='row' gap={2} alignItems='center'>
+					<GiFlexibleLamp size={22} />
+					<span>Monday - Friday: 9:00 AM - 6:00 PM</span>
+				</Stack>
+			</Stack>
+			<SocialSection />
+
+			{/* footer */}
+			<Box
+				sx={{
+					borderTop: '1px solid #f0f0f0',
+					p: 2,
+					mt: 2,
+					fontSize: 14
+				}}
+			>
+				<span>&copy; {currentYear} Karukon. All rights reserved.</span>
+			</Box>
 		</Box>
 	);
 
@@ -102,11 +169,6 @@ const MobileNav = () => {
 
 				{/* buttons */}
 				<Stack direction='row' gap={2} alignItems='center'>
-					{/* wishlist button */}
-					<Box component={Link} href='/wishlist' aria-label='User profile' sx={{ position: 'relative' }}>
-						<Chip label={wishlist} color='primary' size='small' className='absolute top-[-10px] left-[18px]' />
-						<IoIosHeartEmpty size={30} />
-					</Box>
 					{/* cart button */}
 					<Box component={Link} href='/cart' aria-label='User profile' sx={{ position: 'relative' }}>
 						<Chip label={cart} color='primary' size='small' className='absolute top-[-10px] left-[18px]' />
