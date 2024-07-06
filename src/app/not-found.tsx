d13153dc04039ef3900db0047f2c125b19933935
gsjoy24 <gour.joy24@gmail.com>
2024-07-06 17:40:03 +0600

    added loading page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,11 +2,9 @@ import NotFound from '@/assets/not-found.svg';
 import Image from 'next/image';
 const NotFoundPage = () => {
 	return (
-		<div className='flex justify-center items-center py-[50px]'>
-			<div>
-				<Image src={NotFound} alt='page not found' width={300} height={300} />
-				<h1 className='text-2xl font-bold text-center mt-10'>The page you are looking for is not found!</h1>
-			</div>
+		<div className='flex flex-col gap-10 justify-center items-center py-[50px]'>
+			<Image src={NotFound} alt='page not found' width={300} height={300} />
+			<h1 className='text-2xl font-bold text-center'>The page you are looking for is not found!</h1>
 		</div>
 	);
 };
