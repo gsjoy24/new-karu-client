dba30cdbefa850eac3a12cfba5f55bbdc139097b
gsjoy24 <gour.joy24@gmail.com>
2024-08-11 02:40:45 +0600

    fixed layout error

--- src/app/(profile)/layout.tsx
+++ src/app/(profile)/layout.tsx
@@ -1,22 +1,19 @@
 'use client';
-import PrivateRoute from '@/components/Shared/PrivateRoute';
 import { Stack } from '@mui/material';
 import ProfileNav from './components/ProfileNav';
 
 const layout = ({ children }: { readonly children: React.ReactNode }) => {
 	return (
-		<PrivateRoute>
-			<Stack
-				direction={{
-					xs: 'column',
-					md: 'row'
-				}}
-				spacing={2}
-			>
-				<ProfileNav />
-				{children}
-			</Stack>
-		</PrivateRoute>
+		<Stack
+			direction={{
+				xs: 'column',
+				md: 'row'
+			}}
+			spacing={2}
+		>
+			<ProfileNav />
+			{children}
+		</Stack>
 	);
 };
 
