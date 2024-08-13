851d98c7763b8a00fb1b238184157f2d8384c4f6
gsjoy24 <gour.joy24@gmail.com>
2024-08-13 14:29:41 +0600

    updated product api

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,8 +1,27 @@
 'use client';
+import { useGetProductsQuery } from '@/redux/api/productApi';
+import { TQueryParams } from '@/types';
 import { useParams } from 'next/navigation';
+import { useState } from 'react';
 
 const ProductsByCategory = () => {
+	const [filterParam, setFilterParam] = useState({} as TQueryParams);
+	const [searchTerm, setSearchTerm] = useState({} as TQueryParams);
+	const [page, setPage] = useState<number>(1);
 	const { slug } = useParams();
+	const { data, isFetching } = useGetProductsQuery([
+		{
+			name: 'page',
+			value: page
+		},
+		{
+			name: 'category',
+			value: slug
+		}
+	]);
+
+	const totalPage = data?.meta?.total / data?.meta?.limit;
+
 	return (
 		<div>
 			<h1>This is ProductsByCategory {slug}</h1>
--- src/redux/api/productApi.ts
+++ src/redux/api/productApi.ts
@@ -1,4 +1,5 @@
 import { reduxTagTypes } from '@/constants/reduxTagTypes';
+import { TQueryParams } from '@/types';
 import baseApi from './baseApi';
 
 const productApis = baseApi.injectEndpoints({
@@ -10,21 +11,30 @@ const productApis = baseApi.injectEndpoints({
 			}),
 			providesTags: [reduxTagTypes.categories]
 		}),
-		getProductsByCategory: build.query({
-			query: (query: Record<string, unknown>) => ({
-				url: '/products',
-				method: 'GET'
-			}),
-			providesTags: [reduxTagTypes.products]
+		getProducts: build.query({
+			query: (args) => {
+				const params = new URLSearchParams();
+				params.append('limit', '16');
+				args?.forEach((param: TQueryParams) => {
+					params.append(param.name, param.value);
+				});
+				return {
+					url: `/products`,
+					method: 'GET',
+					params
+				};
+			},
+			providesTags: [reduxTagTypes.product]
 		}),
 		getProductBySlug: build.query({
-			query: (slug: string) => ({
-				url: `/products/slug/${slug}`,
+			query: (slug) => ({
+				url: `/products/${slug}`,
 				method: 'GET'
 			}),
+
 			providesTags: [reduxTagTypes.product]
 		})
 	})
 });
 
-export const { useGetNewArrivalsQuery, useGetProductBySlugQuery } = productApis;
+export const { useGetNewArrivalsQuery, useGetProductsQuery, useGetProductBySlugQuery } = productApis;
