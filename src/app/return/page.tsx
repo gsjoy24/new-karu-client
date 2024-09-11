9d75cb34ab334ca8fc0ada4f0cb5169cdc0409d0
gjsoy24 <gour.joy24@gmail.com>
2024-09-11 15:49:10 +0600

    added return policy page

--- src/app/return/page.tsx
+++ src/app/return/page.tsx
@@ -1,9 +1,181 @@
-const ReturnPage = () => {
+import { Box, Container, Divider, Typography } from '@mui/material';
+const ReturnPolicyPage = () => {
 	return (
-		<div>
-			<h1>This is ReturnPage component</h1>
-		</div>
+		<Container maxWidth='md' sx={{ py: 4 }}>
+			{/* Header */}
+			<Box mb={4}>
+				<Typography variant='h4' component='h1' sx={{ fontWeight: 'bold', mb: 2 }}>
+					Return Policy
+				</Typography>
+				<Typography variant='body2' sx={{ mb: 2 }}>
+					Before initiating a return or replacement, please carefully read through our policy to ensure that your
+					purchased item is eligible.
+				</Typography>
+				<Divider />
+
+				<Typography variant='h5' component='h2' sx={{ mb: 2, fontWeight: 'bold' }}>
+					Procedure to Return a Product
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					You have 7 calendar days after the item is delivered to notify us of your intent to return the product.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 2 }}>
+					To initiate a return, please contact our Customer Service at <strong>01700654618</strong>. If your item meets
+					all the return requirements, the return process will be started.
+				</Typography>
+				<Divider sx={{ my: 2 }} />
+
+				<Typography variant='h5' component='h2' sx={{ mb: 2, fontWeight: 'bold' }}>
+					Replacement Guarantee
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					We offer a 7 Days Replacement Guarantee (excluding items in an intact box or packet) for physical products
+					sold on KarukonBd.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					The replacement request must be made within 7 days of receiving the delivery.
+				</Typography>
+				<Typography variant='body1'>
+					Refunds are applicable only for canceled order items due to product unavailability.
+				</Typography>
+				<Divider sx={{ my: 2 }} />
+
+				<Typography variant='h5' component='h2' sx={{ mb: 2, fontWeight: 'bold' }}>
+					Refund Process
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					If we are unable to deliver your order within 10 days or due to product unavailability, we will refund your
+					payment within 10 days through the original payment method. We will also cover the gateway charges associated
+					with the refund.
+				</Typography>
+				<Divider sx={{ my: 2 }} />
+
+				<Typography variant='h5' component='h2' sx={{ mb: 2, fontWeight: 'bold' }}>
+					Requirements for a Valid Return
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Proof of purchase (order number, invoice, etc.).
+				</Typography>
+				<Typography variant='body1'>
+					The reason for return must be valid, and the return conditions must be met.
+				</Typography>
+				<Divider sx={{ my: 2 }} />
+
+				<Typography variant='h5' component='h2' sx={{ mb: 2, fontWeight: 'bold' }}>
+					Valid Conditions and Reasons to Return an Item
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Delivery of the wrong product.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Delivery of a defective product.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Delivery of a product with missing parts.
+				</Typography>
+				<Typography variant='body1'>Incorrect content on the website.</Typography>
+				<Typography variant='body2' sx={{ mt: 2 }}>
+					<strong>Note</strong>: Change of mind is not an applicable reason for return.
+				</Typography>
+				<Divider sx={{ my: 2 }} />
+
+				<Typography variant='h5' component='h2' sx={{ mb: 2, fontWeight: 'bold' }}>
+					Installation Service Terms
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					For products that require installation or fitting, the installation service will be provided within 3 working
+					days from the date the product is received.
+				</Typography>
+				<Divider sx={{ my: 2 }} />
+
+				<Typography variant='h5' component='h2' sx={{ mb: 2, fontWeight: 'bold' }}>
+					Cancellation & Refund Policy Due to Product Unavailability
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Delivery of ordered items is subject to the availability of the products at our warehouse or store. KarukonBd
+					reserves the right to cancel any order within 4 to 7 working days if the stock of the ordered item(s) is
+					temporarily unavailable.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					If the customer agrees to wait for the product(s) to be restocked, the order may remain in an open,
+					processing, or hold status for a maximum of 30 days.
+				</Typography>
+
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					If the order is canceled due to product unavailability and the customer has made an advance payment, a full
+					refund will be processed within 1-10 working days, provided no cashback has been received. If cashback was
+					received, the amount will be adjusted against the refund.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					If the customer cancels an order after confirmation and has made an online payment, karukonbd will charge an
+					Online Gateway Transaction fee when processing the refund.
+				</Typography>
+
+				<Typography variant='body1'>
+					Advance payment is required for the delivery of certain products. Karukonbd reserves the right to cancel any
+					order if the required advance payment is not made.
+				</Typography>
+
+				<Divider sx={{ my: 2 }} />
+
+				<Typography variant='h5' component='h2' sx={{ mb: 2, fontWeight: 'bold' }}>
+					Return/Replacement Guarantee Exclusions
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					The Return/Replacement Guarantee may not apply under the following conditions:
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Damages caused by misuse of the product.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Incidental damage due to malfunctioning of the product.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Any consumable item that has been used or installed.
+				</Typography>
+
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Innerwear, lingerie, socks, and clothing freebies.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Products with tampered or missing serial/UPC numbers.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Any damage or defect not covered under the manufacturer&lsquo;s warranty.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					Any product returned without all original packaging and accessories, including the box, manufacturer&lsquo;s
+					packaging (if any), and all other items originally included with the delivered product(s).
+				</Typography>
+				<Typography variant='body2' sx={{ mt: 2 }}>
+					<strong>Note</strong>: Replacement of the product is subject to the availability of the same on KarukonBd.
+				</Typography>
+				<Divider sx={{ my: 2 }} />
+
+				<Typography variant='h5' component='h2' sx={{ mb: 2, fontWeight: 'bold' }}>
+					Same Day Delivery
+				</Typography>
+
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					<strong>Eligibility</strong>: This service is only applicable for selected product orders delivered within
+					Dhaka city.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					<strong>Delivery Charge</strong>: A delivery charge of 100 takas applies for same-day delivery.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					<strong>Order Timing</strong>: To qualify for same-day delivery, orders must be placed before 3:00 PM.
+				</Typography>
+				<Typography variant='body1' sx={{ mb: 1 }}>
+					<strong>Product Availability</strong>: Only products stored at our office are eligible for same-day delivery.
+				</Typography>
+				<Typography variant='body1'>
+					<strong>Order Cancellation</strong>: Customers have the option to request the cancellation of their order if
+					they do not agree with the same-day delivery terms and conditions.
+				</Typography>
+			</Box>
+		</Container>
 	);
 };
 
-export default ReturnPage;
+export default ReturnPolicyPage;
